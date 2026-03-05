ae44115f4d2f6c290c7800a1de9077d13c15831e
Eliab Campos <campeao.mkt.user@gmail.com>
2026-03-05 12:19:08 -0400

    ajuste db

--- src/database/db.js
+++ src/database/db.js
@@ -2,6 +2,8 @@ const Database = require("better-sqlite3");
 
 const db = new Database("./src/database/leads.db");
 
+/* ================= CRIAR TABELA ================= */
+
 db.prepare(`
 CREATE TABLE IF NOT EXISTS leads (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -17,5 +19,15 @@ CREATE TABLE IF NOT EXISTS leads (
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 )
 `).run();
+
+/* ================= GARANTIR COLUNAS ================= */
+
+try {
+    db.prepare(`ALTER TABLE leads ADD COLUMN comprou INTEGER DEFAULT 0`).run();
+} catch (e) {}
+
+try {
+    db.prepare(`ALTER TABLE leads ADD COLUMN mensagem_enviada INTEGER DEFAULT 0`).run();
+} catch (e) {}
 
 module.exports = db;
